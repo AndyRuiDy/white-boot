b1eab39a01d73f28048335ae8604242cce6fdb1f
zhush <andyjavadream@163.com>
2020-03-11 14:35:32 +0800

    first commit white-boot

---
+++ blog-vue/src/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="test">
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      text: 'success'
+    }
+  }
+}
+</script>
+
+<style>
+#test{
+  color: red;
+  font-size: 56px;
+}
+</style>
